03e3407161b8cef441f13b09c054ccf0a2846ac4
donal <donalspring@gmail.com>
2018-08-30 16:02:33 +0200

    FIX broken test

--- server/app.js
+++ server/app.js
@@ -20,20 +20,16 @@ const app = express();
 const server = require('http').createServer(app);
 require('./config/express')(app);
 
-if (config.mocks && config.mocks.api) {
-  //add stubs if needed
-  require('./mocks/mock-routes')(app);
-} else {
-  const mongoose = require('mongoose');
-  // Connect to database
-  mongoose.connect(config.mongo.uri, config.mongo.options);
-  mongoose.connection.on('error', function(err) {
-      console.error('MongoDB connection error: ' + err);
-      process.exit(-1);
-    }
-  );
-  require('./routes')(app);
-}
+
+const mongoose = require('mongoose');
+// Connect to database
+mongoose.connect(config.mongo.uri, config.mongo.options);
+mongoose.connection.on('error', function(err) {
+    console.error('MongoDB connection error: ' + err);
+    process.exit(-1);
+  }
+);
+require('./routes')(app);
 
 // Start server
 server.listen(config.port, config.ip, function () {
