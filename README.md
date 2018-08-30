d4857f6ef42c4130ec397b78f5a2372658834c92
donal <donalspring@gmail.com>
2018-08-30 12:43:39 +0200

    INIT with scaffold example api

---
+++ README.md
@@ -0,0 +1,10 @@
+# todolist-api
+API backend built in NodeJS using Express to add TODOs to a MongoDB. 
+
+## Deploy on OpenShift
+1. `cd .openshift-applier`
+1.  
+
+
+## Build and Run
+`npm run dev`
---
+++ server/config/express.js
@@ -0,0 +1,42 @@
+/**
+ * Express configuration
+ */
+
+'use strict';
+
+const express = require('express');
+const morgan = require('morgan');
+const compression = require('compression');
+const bodyParser = require('body-parser');
+const methodOverride = require('method-override');
+const cookieParser = require('cookie-parser');
+const errorHandler = require('errorhandler');
+const path = require('path');
+const config = require('./environment');
+
+module.exports = function(app) {
+  const env = app.get('env');
+
+  app.set('views', config.root + '/server/views');
+  app.engine('html', require('ejs').renderFile);
+  app.set('view engine', 'html');
+  app.use(compression());
+  app.use(bodyParser.urlencoded({ extended: false }));
+  app.use(bodyParser.json());
+  app.use(methodOverride());
+  app.use(cookieParser());
+  if ('production' === env  || 'si' === env  || 'ci' === env) {
+    app.use(express.static(path.join(config.root, 'public')));
+    app.set('appPath', path.join(config.root, 'public'));
+    app.use(morgan('dev'));
+  }
+
+  if ('development' === env || 'test' === env) {
+    app.use(require('connect-livereload')());
+    app.use(express.static(path.join(config.root, '.tmp')));
+    app.use(express.static(path.join(config.root, 'client')));
+    app.set('appPath', path.join(config.root, 'client'));
+    app.use(morgan('dev'));
+    app.use(errorHandler()); // Error handler - has to be last
+  }
+};
